5290768dfdce739411e8bb54dd031ac997153e6f
Aymen Djellal <aydjella@microsoft.com>
2020-10-04 04:09:28 -0700

    Remove CBT stuff

--- .config/InPath/build.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set __scriptDir=%~dp0%..\PsModules
-powershell -ExecutionPolicy bypass -Command "$psm1p = Join-Path %__scriptDir% DeveloperHelpers.psm1; Import-Module $psm1p ; Invoke-MSBuild"
