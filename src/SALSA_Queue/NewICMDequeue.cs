273514de73409a1c1e7de9f4ee80ba673b006684
Aymen Djellal <aydjella@microsoft.com>
2020-10-06 15:48:51 -0700

    Adding listener to the Process

--- src/SALSA_Queue/NewICMDequeue.cs
+++ src/SALSA_Queue/NewICMDequeue.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -9,11 +10,43 @@ namespace SALSA_Queue
 {
     public static class NewICMDequeue
     {
+        static TraceWriter Log;
         [FunctionName("NewICMDequeue")]
-        public static void Run([QueueTrigger("salsaqueue", Connection = "")]string myQueueItem, TraceWriter log)
+        public static void Run([QueueTrigger("salsaqueue", Connection = "AzureWebJobsStorage")]string myQueueItem, TraceWriter log)
         {
             var currentDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            Process.Start(Path.Combine(currentDir, @"..\SALsA_Exe.exe"), myQueueItem);
+            var salsaPath = Path.Combine(Directory.GetParent(currentDir).FullName, @"SALsA_Exe\SALsA_Exe.exe");
+
+            var logManager = new LogManager(log);
+
+            var process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.OutputDataReceived += logManager.p_OutputDataReceived;
+            process.ErrorDataReceived += logManager.p_ErrorDataReceived;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.FileName = salsaPath;
+            process.StartInfo.Arguments = myQueueItem;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+        }
+        public class LogManager
+        { 
+            public LogManager(TraceWriter _log)
+            {
+                this.Log = _log;
+            }
+            TraceWriter Log;
+            public void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+            {
+                Log.Info(e.Data);
+            }
+            public void p_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+            {
+                Log.Error(e.Data);
+            }
         }
     }
 }
