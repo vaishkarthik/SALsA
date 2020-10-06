b0e5776e731608cfb5dbd67bde3e09898f2f663c
Aymen Djellal <aydjella@microsoft.com>
2020-10-06 15:16:21 -0700

    Adding Exe to Queue

--- src/SALSA_Queue/NewICMDequeue.cs
+++ src/SALSA_Queue/NewICMDequeue.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -10,7 +12,8 @@ namespace SALSA_Queue
         [FunctionName("NewICMDequeue")]
         public static void Run([QueueTrigger("salsaqueue", Connection = "")]string myQueueItem, TraceWriter log)
         {
-            Process.Start("SALSA_Function.exe", myQueueItem);
+            var currentDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Process.Start(Path.Combine(currentDir, @"..\SALsA_Exe.exe"), myQueueItem);
         }
     }
 }
